44191e18ffd02ea556d2c0ad820c7f2d7247711e
Rachel <bui.rachel@gmail.com>
2015-05-24 16:19:43 +1000

    Changed the HTML

--- ingeneous_site/urls.py
+++ ingeneous_site/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'ingeneous_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'', include('story.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
