b67ff5a607bfa8db63af81de0f535cdd76f30906
Rachel <bui.rachel@gmail.com>
2015-05-31 01:11:15 +1000

    Added views to create stories inside the website.

--- story/urls.py
+++ story/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from . import views
 
+#pk is primary key that refers back to view
+
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^story/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
